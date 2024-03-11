cd063c21ac37f7169149524f39c4cf70361850f6
Elodie Jance <elodie.jance@gmail.com>
2024-03-11 18:17:01 +0100

    installation react router + fichier json

--- KASA-IMMO/src/main.jsx
+++ KASA-IMMO/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<home />} />
+        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/fichelogement" element={<Fichelogement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+);
