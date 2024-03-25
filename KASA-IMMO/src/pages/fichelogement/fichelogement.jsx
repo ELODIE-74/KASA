486275bc7032ad9f6ee9e079c8e7df9d5c3f9bda
Elodie Jance <elodie.jance@gmail.com>
2024-03-25 15:50:41 +0100

    importation  title/pictures+name host + tags logem

--- KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
+++ KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
@@ -7,6 +7,7 @@ import DataLogement from "../../data/logements.json";
 
 //import composants
 import Slide from "../../components/slide/slide";
+
 //fonction pour gérer les évènements et éléments de la page logement
 function Fichelogement() {
   // Récupérer l'ID du logement à partir de l'URL
@@ -25,7 +26,22 @@ function Fichelogement() {
 
   return (
     <div className="ficheslogement">
-      {logement && <Slide pictures={logement.pictures} />}
+      {logement && (
+        <>
+          <Slide pictures={logement.pictures} />
+          <h2>{logement.title}</h2>
+          <div>
+            <h3>{logement.host.name}</h3>
+            <img className="imgcercle" src={logement.host.picture} />
+          </div>
+          <p>{logement.location}</p>
+          <ul>
+            {logement.tags.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
