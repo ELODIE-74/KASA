e60ba3d0cf9c07f384b8bdcf95a3e08274d6472b
Elodie Jance <elodie.jance@gmail.com>
2024-03-25 14:15:26 +0100

    essai slide avec changement couleur dots

--- KASA-IMMO/src/components/slide/slide.jsx
+++ KASA-IMMO/src/components/slide/slide.jsx
@@ -3,21 +3,32 @@ import "./slide.css";
 import leftArrow from "../../assets/arrowleft.png";
 import rightArrow from "../../assets/arrowright.png";
 
+//fonction pour gérer l'affichage des images du logement
 function Slide({ pictures }) {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [activeDot, setActiveDot] = useState(0);
 
+  //gérer la slide + la dot passé + retour arrière
   const handlePrevSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === 0 ? pictures.length - 1 : prevSlide - 1
     );
+    setActiveDot((prevDot) =>
+      prevDot === 0 ? pictures.length - 1 : prevDot - 1
+    );
   };
 
+  //gére la slide + la dot pour aller à celle d'après +1
   const handleNextSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === pictures.length - 1 ? 0 : prevSlide + 1
     );
+    setActiveDot((prevDot) =>
+      prevDot === pictures.length - 1 ? 0 : prevDot + 1
+    );
   };
 
+  //return au clic sur la flèche gauche l'image et la dot d'avant
   return (
     <div className="slideshow">
       {pictures.length > 1 && (
@@ -46,11 +57,14 @@ function Slide({ pictures }) {
 
       {pictures.length > 1 && (
         <div className="pagination">
-          {pictures.map((index) => (
+          {pictures.map((picture, index) => (
             <div
               key={index}
-              className={`dot ${index === currentSlide ? "active" : ""}`}
-              onClick={() => setCurrentSlide(index)}
+              className={`dot ${index === activeDot ? "active" : ""}`}
+              onClick={() => {
+                setCurrentSlide(index);
+                setActiveDot(index);
+              }}
             />
           ))}
         </div>
