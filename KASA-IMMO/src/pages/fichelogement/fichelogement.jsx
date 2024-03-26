3dad834be3a9f3b6b84fa093a7dc819846bfd333
Elodie Jance <elodie.jance@gmail.com>
2024-03-26 15:59:41 +0100

    police montserrat a jour

--- KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
+++ KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
@@ -10,21 +10,30 @@ import Slide from "../../components/slide/slide";
 //import Collapse from "../../components/collapse/collapse";
 import Rating from "../../components/rating/rating";
 
+//import de la page error en cas de problème d'id
+import "../../pages/error404/error404";
+
 //fonction pour gérer les évènements et éléments de la page logement
 function Fichelogement() {
   // Récupérer l'ID du logement à partir de l'URL
   const [logement, setLogement] = useState(null);
   const { id } = useParams(); // Récupère l'id du logement dans l'url
+  const [isInvalidPropertyId, setIsInvalidPropertyId] = useState(false); //validité ou non de l'id de proprité
 
   useEffect(() => {
-    // Récupère les infos du logement en fonction de l'id
     const data = DataLogement.find((logement) => logement.id === id);
     if (data) {
       setLogement(data);
+      setIsInvalidPropertyId(false);
     } else {
       setLogement(null);
+      setIsInvalidPropertyId(true);
     }
   }, [id]);
+
+  if (isInvalidPropertyId) {
+    return <Redirect to="/erreur404" />;
+  }
 
   return (
     <div className="ficheslogement">
