01cc73331b07b8f78cd3c7aa44d24d4ae5975eb6
Elodie Jance <elodie.jance@gmail.com>
2024-04-08 12:21:07 +0200

    mise a joure page logement + css générale

--- KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
+++ KASA-IMMO/src/pages/fichelogement/fichelogement.jsx
@@ -82,11 +82,17 @@ function Fichelogement() {
            */}
           <div className="collapse-container">
             <div className="collapse-coD" id="collapse-coD">
-              <Collapse title="Description" text={logement.description} />
+              <Collapse
+                title={<span className="collapse-title">Description</span>}
+                text={logement.description}
+              />
             </div>
             <div className="collapse-coE" id="collapse-coE">
               {logement.equipments && (
-                <Collapse title="Équipements" text={logement.equipments} />
+                <Collapse
+                  title={<span className="collapse-title">Équipements</span>}
+                  text={logement.equipments}
+                />
               )}
             </div>
           </div>
