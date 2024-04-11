0b18db408a3ecf9c84cf6181621abed695aae41e
Elodie Jance <elodie.jance@gmail.com>
2024-04-11 11:25:39 +0200

    modification slide chiffres a la place des dots

--- KASA-IMMO/src/components/slide/slide.jsx
+++ KASA-IMMO/src/components/slide/slide.jsx
@@ -3,32 +3,21 @@ import "./slide.css";
 import leftArrow from "../../assets/arrowleft.png";
 import rightArrow from "../../assets/arrowright.png";
 
-//fonction pour gérer l'affichage des images du logement
 function Slide({ pictures }) {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [activeDot, setActiveDot] = useState(0);
 
-  //gérer la slide + la dot passé + retour arrière
   const handlePrevSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === 0 ? pictures.length - 1 : prevSlide - 1
     );
-    setActiveDot((prevDot) =>
-      prevDot === 0 ? pictures.length - 1 : prevDot - 1
-    );
   };
 
-  //gére la slide + la dot pour aller à celle d'après +1
   const handleNextSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === pictures.length - 1 ? 0 : prevSlide + 1
     );
-    setActiveDot((prevDot) =>
-      prevDot === pictures.length - 1 ? 0 : prevDot + 1
-    );
   };
 
-  //return au clic sur la flèche gauche l'image et la dot d'avant
   return (
     <div className="slideshow">
       {pictures.length > 1 && (
@@ -54,19 +43,20 @@ function Slide({ pictures }) {
           onClick={handleNextSlide}
         />
       )}
-      {/**chaque dot représente une image dans le carrousel, l'index de chaque image est utilisé pour déterminer si le dot est actif ou non */}
       {pictures.length > 1 && (
         <div className="pagination">
-          {pictures.map((picture, index) => (
-            <div
-              key={index}
-              className={`dot ${index === activeDot ? "active" : ""}`}
-              onClick={() => {
-                setCurrentSlide(index);
-                setActiveDot(index);
-              }}
-            />
-          ))}
+          <div className="pagination-text">
+            {`${currentSlide + 1}/${pictures.length}`}
+          </div>
+          <div className="chiffres">
+            {pictures.map((index) => (
+              <div
+                key={index}
+                className={`chiffre ${index === currentSlide ? "active" : ""}`}
+                onClick={() => setCurrentSlide(index)}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
